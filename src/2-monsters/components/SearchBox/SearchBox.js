ba6cd2724d06964b362c70e6bccd564084da106f
DongKyoung <e1loldan@gmail.com>
2020-06-16 19:39:03 +0900

    Monster 과제 제출

--- src/2-monsters/components/SearchBox/SearchBox.js
+++ src/2-monsters/components/SearchBox/SearchBox.js
@@ -2,16 +2,9 @@ import React, { Component } from "react";
 import "./SearchBox.css";
 
 class SearchBox extends Component {
-  render() {
-    return (
-      <input
-        className="search"
-        type="search"
-        placeholder="Search..."
-        onChange={this.props.handleChange}
-      />
-    );
-  }
+    render() {
+        return <input className="search" type="search" placeholder="Search..." onChange={this.props.handleChange} />;
+    }
 }
 
 export default SearchBox;
